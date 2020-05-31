80e02a0e3ea9770984647c343377ff639f09eb9f
CleanCrazy <yuraivanov.dev@gmail.com>
2020-05-31 19:58:47 +1000

    added Invetories screen and replaced HomePage Screen

--- src/Sections/index.js
+++
@@ -1,7 +0,0 @@
-import Communication from './Communication';
-import Home from './Home';
-import Rto from './Rto';
-import Support from './Support';
-const HomePage=Home;
-
-export { HomePage, Support, Rto, Communication };
