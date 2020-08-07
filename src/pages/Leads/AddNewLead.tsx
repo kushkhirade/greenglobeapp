f04793e9f5e3300bce23299dd1b875fb62b66eb8
CleanCrazy <yuraivanov.dev@gmail.com>
2020-08-07 15:08:54 +0300

    fixed update lead

--- src/pages/Leads/AddNewLead.tsx
+++ src/pages/Leads/AddNewLead.tsx
@@ -109,6 +109,7 @@ export class AddNewLeadImpl extends React.Component<
     id: number;
     complainCheckList: any;
     dealerCheckboxesChanged: boolean;
+    currentInsertEmail: string;
   }
 > {
   constructor(props: IAddNewLeadProps) {
@@ -120,6 +121,7 @@ export class AddNewLeadImpl extends React.Component<
       allTasks: [],
       id: 0,
       dealerCheckboxesChanged: false,
+      currentInsertEmail: '',
       complainCheckList: {
         "Low Average / Mileage": false,
         "Late Starting Problem": false,
@@ -400,6 +402,144 @@ export class AddNewLeadImpl extends React.Component<
     };
     this.setState({ dealerCheckboxes: dealerCheckboxesData });
     changeValuesInStore(formType, editData);
+  };
+
+  insertInDistStep1 = async (userForm) => {
+    const currentUser = getToken().data;
+    const {
+      firstName,
+      middleName,
+      lastName,
+      email,
+      company,
+      whatsAppNumber,
+      leadType,
+      leadSource,
+      leadStatus,
+      subLeadSource,
+      rating,
+      street,
+      city,
+      state,
+      zip,
+      country,
+    } = userForm;
+    const query = `INSERT INTO salesforce.Lead (FirstName,MiddleName,LastName,Email,Company,Whatsapp_number__c,Lead_Type__c,LeadSource,Status,Sub_Lead_Source__c,Rating,Street,City,State,PostalCode,Country,RecordTypeId,Assigned_Distributor__c)
+    VALUES ('${firstName ?? ""}','${middleName ?? ""}','${lastName ?? ""}','${
+      email ?? ""
+    }','${company ?? ""}',${whatsAppNumber ? whatsAppNumber : ""},'${
+      leadType ?? ""
+    }', '${leadSource ?? ""}','${leadStatus ?? ""}','${subLeadSource ?? ""}','${
+      rating ?? ""
+    }','${street ?? ""}','${city ?? ""}','${state ?? ""}','${zip ?? ""}','${
+      country ?? ""
+    }', '0122w000000chRuAAI', '${currentUser.sfid}')`;
+
+    try {
+      const result = await getData({
+        query,
+        token: currentUser.token,
+      });
+      console.log(result);
+      if (
+        typeof result === "object" &&
+        result.name &&
+        result.name === "SyntaxError"
+      ) {
+        throw result;
+      }
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  insertDealerStep1 = async (userForm) => {
+    const currentUser = getToken().data;
+    const {
+      firstName,
+      middleName,
+      lastName,
+      email,
+      company,
+      whatsAppNumber,
+      leadType,
+      leadSource,
+      leadStatus,
+      subLeadSource,
+      rating,
+      street,
+      city,
+      state,
+      zip,
+      country,
+      vehicleNumber,
+      fuelType,
+      wheeles,
+      vehicleMek,
+      vehicleModel,
+      usage,
+      vehicleType,
+      dailyRunning,
+      registration,
+      mfg,
+      chassis,
+      gstNumber,
+    } = userForm;
+    const query = `INSERT INTO salesforce.Lead (FirstName, MiddleName, LastName, Company,Email,Whatsapp_number__c,Lead_Type__c,LeadSource,Status,Sub_Lead_Source__c ,Rating,  Street,  City ,State ,PostalCode,Country, Vehicle_no__c,Fuel_Type__c,X3_or_4_Wheeler__c,Vehicle_Make__c, Vehicle_Model__c,Usage_of_Vehicle__c,Engine__c, Daily_Running_Kms__c,Registration_Year__c,Year_of_Manufacturing__c,Chassis_No__c,  GST_Number__c , RecordTypeId, Assigned_Dealer__c) 
+    VALUES    
+    ('${firstName ?? ""}','${middleName ?? ""}','${lastName ?? ""}','${
+      email ?? ""
+    }','${company ?? ""}',${whatsAppNumber ? whatsAppNumber : 0},'${
+      leadType ?? ""
+    }', '${leadSource ?? ""}','${leadStatus ?? ""}','${subLeadSource ?? ""}','${
+      rating ?? ""
+    }','${street ?? ""}','${city ?? ""}','${state ?? ""}','${zip ?? ""}','${
+      country ?? ""
+    }', '${vehicleNumber ?? ""}','${fuelType ?? ""}','${wheeles ?? ""}','${
+      vehicleMek ?? ""
+    }','${vehicleModel ?? ""}','${usage ?? ""}','${vehicleType ?? ""}',
+       ${dailyRunning ? dailyRunning : 0},'${
+      registration ? registration : "4/5/2019"
+    }',${mfg ? mfg : 0},'${chassis ?? ""}','${
+      gstNumber ?? ""
+    }', '0122w000000chRuAAI', '${currentUser.sfid}') RETURNING email`;
+
+    try {
+      const result = await getData({
+        query,
+        token: currentUser.token,
+      });
+      console.log(result);
+      if (
+        typeof result === "object" &&
+        result.name &&
+        result.name === "SyntaxError"
+      ) {
+        throw result;
+      }
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  insertDealerStep = async (status) => {
+    const currentUser = getToken().data;
+    const { currentInsertEmail } = this.state;
+    console.log(this.state)
+    const query = `select sfid from salesforce.lead where email = '${currentInsertEmail}' `;
+    const result = await getData({
+      query,
+      token: currentUser.token,
+    });
+    console.log(result);
+    /* const statusQuery = `UPDATE salesforce.lead set Status = '${status}' WHERE  sfid = '0010l000017kOHr'`;
+    const resultStatusQuery = await getData({
+      query: statusQuery,
+      token: currentUser.token,
+    });
+    console.log(resultStatusQuery); */
   };
 
   InsertNewTask = async (data, leadTaskForm, id) => {
@@ -801,11 +941,16 @@ export class AddNewLeadImpl extends React.Component<
           />
           <SubFormHeading>Vehicle Details</SubFormHeading>
           <FormComponent
-            onSubmit={(v: any) => {
-              this.setState({
-                activeStep: this.state.activeStep + 1,
-              });
-              console.log(">> v", v);
+            onSubmit={async (v: any) => {
+              try {
+                const result = await this.insertDealerStep1(v);
+                this.setState({
+                  currentInsertEmail: 'abac@gamil.com',
+                  activeStep: this.state.activeStep + 1,
+                });
+              } catch (error) {
+                console.log(error);
+              }
             }}
             formModel="leadForm"
             hasSubmit={true}
@@ -854,8 +999,9 @@ export class AddNewLeadImpl extends React.Component<
           <UploadContainer valKey={7} heading="Aadhaar Card" />
           <UploadContainer valKey={8} heading="PAN Card" />{" "}
           <FormComponent
-            onSubmit={(v: any) => {
+            onSubmit={async (v: any) => {
               console.log(">> v", v);
+              await this.insertDealerStep('Document Collection');
               this.setState({
                 activeStep: this.state.activeStep + 1,
               });
@@ -907,7 +1053,8 @@ export class AddNewLeadImpl extends React.Component<
           </div>
         </div>{" "}
         <FormComponent
-          onSubmit={(v: any) => {
+          onSubmit={async (v: any) => {
+            await this.insertDealerStep('Negotiation');
             this.setState({
               activeStep: this.state.activeStep + 1,
             });
@@ -959,8 +1106,9 @@ export class AddNewLeadImpl extends React.Component<
           options={{ responsive: "scrollMaxHeight" }}
         />{" "}
         <FormComponent
-          onSubmit={(v: any) => {
+          onSubmit={async(v: any) => {
             console.log(">> v", v);
+            await this.insertDealerStep('Closed');
             this.setState({
               activeStep: this.state.activeStep + 1,
             });
@@ -1008,7 +1156,13 @@ export class AddNewLeadImpl extends React.Component<
               return (
                 <React.Fragment>
                   <Grid
-                    className="checkbox-container" item xs={6} md={6} lg={6} sm={6}>
+                    className="checkbox-container"
+                    item
+                    xs={6}
+                    md={6}
+                    lg={6}
+                    sm={6}
+                  >
                     <div
                       style={{
                         display: "flex",
@@ -1044,7 +1198,14 @@ export class AddNewLeadImpl extends React.Component<
               const isChecked = this.state.dealerCheckboxes[key];
               return (
                 <React.Fragment>
-                  <Grid className="checkbox-container" item xs={6} md={6} lg={6} sm={6}>
+                  <Grid
+                    className="checkbox-container"
+                    item
+                    xs={6}
+                    md={6}
+                    lg={6}
+                    sm={6}
+                  >
                     <div
                       style={{
                         display: "flex",
@@ -1089,7 +1250,7 @@ export class AddNewLeadImpl extends React.Component<
         <FormComponent
           onSubmit={(v: any) => {
             console.log(">> v", v);
-            this.handleLeadDealerSubmit();
+            //this.handleLeadDealerSubmit();
           }}
           formModel="leadForm"
           hasSubmit={true}
@@ -1418,11 +1579,16 @@ export class AddNewLeadImpl extends React.Component<
                         <UploadContainer valKey={7} heading="Aadhaar Card" />
                         <UploadContainer valKey={8} heading="PAN Card" />{" "}
                         <FormComponent
-                          onSubmit={(v: any) => {
+                          onSubmit={async (v: any) => {
                             console.log(">> v", v);
-                            this.setState({
-                              activeStep: this.state.activeStep + 1,
-                            });
+                            try {
+                              await this.insertInDistStep1(v);
+                              this.setState({
+                                activeStep: this.state.activeStep + 1,
+                              });
+                            } catch (error) {
+                              console.log(error);
+                            }
                           }}
                           formModel="userForm"
                           hasSubmit={true}
@@ -1454,7 +1620,6 @@ export class AddNewLeadImpl extends React.Component<
                               activeStep: this.state.activeStep + 1,
                             });
                             console.log(">> v", v);
-                            this.handleLeadDistributorSubmit();
                           }}
                           formModel="userForm"
                           hasSubmit={true}
